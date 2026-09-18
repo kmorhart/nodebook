ed73155ab270b09b4210e061a67b375fe8417784
kmorhart <35631494+kmorhart@users.noreply.github.com>
2026-09-17 23:36:09 -0400

    refactor: remove redundant code

--- apps/app/src/utils/auth.ts
+++ apps/app/src/utils/auth.ts
@@ -1,8 +1,6 @@
 import { redirect, type MiddlewareFunction } from 'react-router';
-import { type Me } from './types';
-import { userContext } from './context';
 import { queryClient } from '../query/queryClient';
-import { meQueryOptions, sessionQueryOptions } from '../query/authQueries';
+import { sessionQueryOptions } from '../query/authQueries';
 
 export const verifySession: MiddlewareFunction = async (args, next) => {
     const isAuthPage = args.request.url.includes('/login') || args.request.url.includes('/register')
@@ -11,24 +9,6 @@ export const verifySession: MiddlewareFunction = async (args, next) => {
         const userUuid = await queryClient.query({ ...sessionQueryOptions, staleTime: 30 * 1000 });
 
         if(isAuthPage && userUuid) return redirect("/new");
-
-        return await next();
-    } catch (error) {
-        if (isAuthPage) return await next()
-        
-        return redirect("/login");
-    }
-}
-
-export const verifyMe: MiddlewareFunction = async (args, next) => {
-    const isAuthPage = args.request.url.includes('/login') || args.request.url.includes('/register')
-
-    try {
-        const me = await queryClient.query({ ...meQueryOptions, staleTime: 5 * 60 * 1000 });
-
-        args.context.set(userContext, me as Me);
-
-        if(isAuthPage && me) return redirect("/new");
 
         return await next();
     } catch (error) {
--- apps/app/src/utils/context.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react-router";
-import type { Me } from "./types";
-
-export const userContext = createContext<Me | null>(null);
